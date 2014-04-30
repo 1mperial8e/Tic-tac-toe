d0b640195cd327478ef1c7d2209ac4611d8d38b0
Misha <misha2g@m2g.local>
2014-04-30 15:45:45 +0300

    Маленькі зміни

---
+++ Tic tac toe/View Controllers/TTGameField.h
@@ -0,0 +1,12 @@
+//
+//  TTGameField.h
+//  Tic tac toe
+//
+//  Created by Misha on 30.04.14.
+//  Copyright (c) 2014 mobilesoft365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTGameField : UIView
+@end
---
+++ Tic tac toe/View Controllers/TTGameField.m
@@ -0,0 +1,32 @@
+//
+//  TTGameField.m
+//  Tic tac toe
+//
+//  Created by Misha on 30.04.14.
+//  Copyright (c) 2014 mobilesoft365. All rights reserved.
+//
+
+#import "TTGameField.h"
+
+@implementation TTGameField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        NSLog(@"load");
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
